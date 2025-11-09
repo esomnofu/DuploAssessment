e047b81657b1728eb0823da6ba755c22755daea6
Mac <mac@MACs-MacBook-Pro.local>
2025-11-09 22:03:06 +0100

    ref: added firebase pubs

--- duplo/lib/main.dart
+++ duplo/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:duplo/modules/onboarding/splash_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   loadEnv();
   runApp(const MyApp());
 }
